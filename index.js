acc57735cf5de6a63ebc72f067e91204cf3070ae
Daniel Skelton <daniel.skelton@rehabstudio.com>
2016-04-17 16:08:47 +0100

    fixed example

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const api = require('./src/rehab-api')
 
 //find all the articles within the first two pages by Creative Technologists
 
-var chained = api.articles(2)
+var creative_tech_articles = api.articles(2)
             .map(api.findBy({author: {description: 'Creative Technologist'}}))
 
 
 //run the computation
-hacks.fork(
+creative_tech_articles.fork(
     (err) => console.log('ERROR:', err)
   , (done) => console.log(
                 'Articles written by Creative Technologists include:'
